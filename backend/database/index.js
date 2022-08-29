b16f3db9e6635406d43f699dbc0c7d02f8afca69
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-08-29 22:42:21 +0530

    Update index.js

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDatabase() {
-  const dbUri = "mongodb://localhost:27017/data";
+  const dbUri = "mongodb://localhost:27017/local";
 
   try {
     const response = await mongoose.connect(dbUri);
